c6728607b844bce54e292bbbdc782ed107e703a5
Vinícius M <viniqpoweryt@gmail.com>
2022-09-07 16:29:45 -0300

    Add class "Players"

--- ruby.rb
+++ ruby.rb
@@ -1,5 +1,21 @@
-class Board
+class Player
+  @@players = []
+  
+  def initialize(player)
+      @player = player
+      @slots = []
+      @@players << self
+  end
 
+  def play(move, board)
+    @slots << move.to_sym
+    board.edit_board(move)
+  end
+end
+
+class Board
+  attr_writer :board
+  
   def initialize
     @board = { 
       one: 1,
@@ -13,13 +29,28 @@ class Board
       nine: 9,
     }
   end
+  
   def show_board
     puts "#{@board[:one]}|#{@board[:two]}|#{@board[:three]}"
     puts "#{@board[:four]}|#{@board[:five]}|#{@board[:six]}"
     puts "#{@board[:seven]}|#{@board[:eight]}|#{@board[:nine]}"
   end
+
+  def edit_board(move)
+    @board[move.to_sym] = "x"
+  end
 end
 
 
-board1 = Board.new
-board1.show_board
+board = Board.new
+player1 = Player.new("player1")
+board.show_board
+puts
+player1.play("one", board)
+board.show_board
+puts
+player1.play("two", board)
+board.show_board
+puts
+player1.play("three", board)
+board.show_board
